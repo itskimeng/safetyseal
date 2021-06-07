0697ddb7d303d69cc3e6ff7460831c53a18d4bd6
Jeck Castillo <breezelback@gmail.com>
2021-06-07 16:03:19 +0800

    files,guidelines
    Add pdf files for guidelines

--- layout/navbar.php
+++ layout/navbar.php
@@ -11,7 +11,7 @@
             <a class="nav-link active" aria-current="page" href="index.html.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Guidelines </a>
+            <a class="nav-link" href="guidelines.php">Guidelines </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Application </a>
---
+++ views/guidelines.php
@@ -0,0 +1,85 @@
+<img src="frontend/images/banner_calabarzon.png" height="10%" width="100%" alt="">
+ <hr>
+  <div class="row mt-5 mb-5">
+    <div class="col-md-12">
+
+      <div class="card">
+        <div class="card-header">
+          Home/Guidelines
+        </div>
+        <div class="card-body" style="background-color: #f9f8f8;">
+
+           <table class="table table-hover mb-0 border-bottom">
+              <tbody>
+                  <tr>
+                      <td>
+                          <div class="h5">
+                              JOINT MEMORANDUM CIRCULAR NO. 21-01 Series of 2021
+                          </div>
+                          <div class="text-muted">
+                              Implementing Guidelines of the
+                              Safety Seal Certification Program
+                          </div>
+                      </td>
+                      <td nowrap="">
+                          <a href="files/guidelines/Joint+Memorandum+Circular+No+.21-01.pdf" target="_blank" class="btn btn-light-primary" rel="noopener noreferrer"><i class="fa fa-file-download"></i> Download</a>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>
+                          <div class="h5">
+                              INGAT ANGAT : Safety Seal Toolkit
+                          </div>
+                      </td>
+                      <td nowrap="">
+                          <a href="files/guidelines/IA+Safety+Seal+Toolkit.pdf" target="_blank" class="btn btn-light-primary" rel="noopener noreferrer"><i class="fa fa-file-download"></i> Download</a>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>
+                          <div class="h5">
+                              MEMORANDUM CIRCULAR NO. 2021-053
+                          </div>
+                          <div class="text-muted">
+                              Implementing Guidelines of the
+                              Safety Seal Certification Program for this Department and the Local Government Units as Issuing
+                              Authorities
+                          </div>
+                      </td>
+                      <td nowrap="">
+                          <a href="files/guidelines/MEMORANDUM+CIRCULAR+NO.+2021-053.pdf" target="_blank" class="btn btn-light-primary" rel="noopener noreferrer"><i class="fa fa-file-download"></i> Download</a>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>
+                          <div class="h5">
+                              DILG Safety Seal Checklist
+                          </div>
+                      </td>
+                      <td nowrap="">
+                          <a href="files/guidelines/Safety+Seal+Certification+Checklist.pdf" target="_blank" class="btn btn-light-primary" rel="noopener noreferrer"><i class="fa fa-file-download"></i> Download</a>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>
+                          <div class="h5">
+                              LGU Safety Seal Checklist
+                          </div>
+                      </td>
+                      <td nowrap="">
+                          <a href="files/guidelines/Safety+Seal+Certification+Checklist+-+LGU.pdf" target="_blank" class="btn btn-light-primary" rel="noopener noreferrer"><i class="fa fa-file-download"></i> Download</a>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+
+        </div>
+      </div>
+
+
+
+
+
+
+    </div><!-- <div class="col-md-12"> -->
+  </div>
