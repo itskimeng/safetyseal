b5abb2a82b16a92ddb159eaca23385a5c2f2ccff
KimPogi <markkimsacluti10101996@gmail.com>
2021-06-07 08:29:17 +0800

    upload codes

---
+++ layout/navbar.php
@@ -0,0 +1,40 @@
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top" style=" background-color: #1e1e2d; ">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Guidelines </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Guidelines </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Application </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Certified Establishments </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Inspection and Certification Teams </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Complaints</a>
+          </li>
+        
+        </ul>
+        <a class="nav-link login-menu" href="#">Login</a>
+
+        <span><a class="nav-link contact-us" href="#" tabindex="-1" aria-disabled="true">Contact Us</a><span>
+
+      </div>
+    </div>
+  </nav>
+</header>
