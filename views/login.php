5f61c1e45b5f831804434696eb70edf985f94065
KimPogi <markkimsacluti10101996@gmail.com>
2021-06-08 16:31:41 +0800

    push all

--- views/login.php
+++ views/login.php
@@ -11,19 +11,45 @@ if(isset($_POST["login"]))
      {  
           $username = mysqli_real_escape_string($conn, $_POST["username"]);  
           $password = md5($_POST['password']);  
-          $query = "SELECT * FROM tbl_userinfo WHERE UNAME = '$username' AND PASSWORD = '$password' and IS_APPROVE = '1'";  
-          echo $query;
+          $sql1 = mysqli_query($conn,"SELECT `ROLE`,`UNAME` FROM tbl_userinfo WHERE UNAME = '".$username."' and IS_APPROVE = '1' LIMIT 1");
+          $row = mysqli_fetch_array($sql1);
+          $role_access = $row['ROLE'];
+        if(empty($role_access))
+        {
+            header('Location: ../registration.php?message=failed');
+        }
+        if($role_access == 'admin'){
+          $query = "SELECT * FROM tbl_userinfo WHERE UNAME = '$username' AND PASSWORD = '$password' and `ROLE`= 'admin' ";  
           $result = mysqli_query($conn, $query);  
           if(mysqli_num_rows($result) > 0)  
           {  
                $_SESSION['username'] = $username;  
-               header("location:../dashboard.php");  
+               header("location: ../dashboard.php?username=".$username."");  
           }  
           else  
           {  
-               echo '<script>alert("Login Failed.!")</script>';  
+               echo '<script>alert("Login Failed.!")</script>'; 
+               header('Location: ../registration.php?message=failed');
           }  
+        }else if($role_access == 'user'){
+            $query = "SELECT * FROM tbl_userinfo WHERE UNAME = '$username' AND PASSWORD = '$password' and `ROLE`= 'user' ";  
+          $result = mysqli_query($conn, $query);  
+          if(mysqli_num_rows($result) > 0)  
+          {  
+               $_SESSION['username'] = $username;  
+               header("location:../dashboard.php?username=".$username."");  
+          }  
+          else  
+          {  
+               echo '<script>alert("Login Failed.!")</script>'; 
+               header('Location: ../registration.php?message=failed ');
+          }  
+        }
+          
      }  
 } 
 mysqli_close($conn);
 ?>
+
+
+ 
