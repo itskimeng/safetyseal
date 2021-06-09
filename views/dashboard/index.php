e3b723371f5fa187583ce0747e06a58d0c74dd1e
sunonin <sodsodjomarie@gmail.com>
2021-06-09 23:18:59 +0800

    test code

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -6,8 +6,8 @@
       </div><!-- /.col -->
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="#">Home</a></li>
-          <li class="breadcrumb-item active">Layout</li>
+          <li class="breadcrumb-item"><a href="dashboard.v2.php">Home</a></li>
+          <li class="breadcrumb-item active">Dashboard</li>
         </ol>
       </div>
     </div>
