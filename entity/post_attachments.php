a68e66c0febf09e095c09d6a1d2656c25454ff35
sunonin <sodsodjomarie@gmail.com>
2021-06-10 15:40:45 +0800

    code refactor

--- entity/post_attachments.php
+++ entity/post_attachments.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 date_default_timezone_set('Asia/Manila');
-// $url_array = explode('?', 'http://'.$_SERVER ['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-$url_array = explode('?', 'http://'.$_SERVER ['HTTP_HOST'].'/safetyseal/');
+$url_array = explode('?', 'http://'.$_SERVER ['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+// $url_array = explode('?', 'http://'.$_SERVER ['HTTP_HOST'].'/safetyseal/');
 $url = $url_array[0];
 
 require_once 'google-api-php-client/src/Google_Client.php';
@@ -14,7 +14,7 @@ require_once 'google-api-php-client/src/contrib/Google_DriveService.php';
 	$client = new Google_Client();
 	$client->setClientId('312607959862-4po30giaf5ft6gk4e214nadae33dp8rl.apps.googleusercontent.com');
 	$client->setClientSecret('i0aX5UG17jovoF2aPgqfoGvS');
-	$client->setRedirectUri('https://localhost/safetyseal/wbstapplication.php');
+	$client->setRedirectUri($url);
 	$client->setScopes(array('https://www.googleapis.com/auth/drive'));
 
 	if (isset($_GET['code'])) {
@@ -68,6 +68,6 @@ require_once 'google-api-php-client/src/contrib/Google_DriveService.php';
 // }
 
 
-    // header('location:../wbstapplication.php');
+    header('location:../wbstapplication.php');
 
 
