3b1d8c17f2553fb49381fb8716c742dcd4f7337e
Jeck Castillo <breezelback@gmail.com>
2021-06-11 11:16:48 +0800

    complaints
    fetch data from conplaints database

--- layout/header.html.php
+++ layout/header.html.php
@@ -11,6 +11,10 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 
+    <!-- datatable css -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/css/dataTables.bootstrap.min.css" /> -->
+
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.15/css/dataTables.jqueryui.min.css"/>
 
 
 
@@ -23,7 +27,20 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
 
-  <link rel="stylesheet" href="frontend/dist/css/skins/_all-skins.min.css">
+    <link rel="stylesheet" href="frontend/dist/css/skins/_all-skins.min.css">
+
+
+    <!-- datatable js -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js"></script> -->
+
+
+
+
+  
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+
+
+    
 
 </head>
 
