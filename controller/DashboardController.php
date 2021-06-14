a14abd7600ee1b82b79ab40ec5224c7d7a09ebf1
sunonin <sodsodjomarie@gmail.com>
2021-06-15 06:26:37 +0800

    add note

--- controller/DashboardController.php
+++ controller/DashboardController.php
@@ -10,7 +10,13 @@ $citymun = $_SESSION['city_mun'];
 
 
 $count_status = $app->countStatus($province, $citymun);
+
+// basic principle ng OOP and best coding practices
+// 'set' is used kapag nagseset ka ng value papunta ng entity/db
+// never gagamitin as prefix ng function
+// palitan mo to
 $provinces_title = $app->setBarChartLabel();
+// ============
 $receiving = $app->getdataForReceived();
 $approved = $app->getdataApproved();
 
