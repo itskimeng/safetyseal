447c6674a8c96b1eda75b24791c02329cf1aa051
Jeck Castillo <breezelback@gmail.com>
2021-06-14 15:58:49 +0800

    Establishment,Certificate
    Dynamic Content

--- views/certified-establishments.php
+++ views/certified-establishments.php
@@ -1,3 +1,7 @@
+<?php require_once 'application/config/connection.php'; ?>
+
+
+
 <img src="frontend/images/banner_calabarzon.png" height="10%" width="100%" alt="">
  <hr>
   <div class="row mt-5 mb-5">
@@ -38,46 +42,66 @@
                 </tr>
             </thead>
             <tbody>
-                <tr class="clickable-row" data-href="establishment-profile.php">
+              <?php 
+              $selectApplication = ' SELECT `id`, `control_no`, `user_id`, `status`, `has_consent`, `date_created`, `date_proceed`, `receiver_id`, `date_received`, `approver_id`, `date_approved`, `safety_seal_no`, `date_modified` FROM `tbl_app_checklist` WHERE status = "Approved" ';
+              $execSelectApplication = $conn->query($selectApplication);
+              while ($resultApplication = $execSelectApplication->fetch_assoc()) 
+              {
+                $selectApplicantDetails = ' SELECT `ID`, `REGION`, `PROVINCE`, `LGU`, `OFFICE`, `CMLGOO_NAME`, `UNAME`, `PASSWORD`, `VERIFICATION_CODE`, `IS_APPROVED`, `IS_VERIFIED`, `ROLES`, `EMAIL` FROM `tbl_admin_info` WHERE `ID` = "'.$resultApplication['user_id'].'" ';
+                $execApplicantDetails = $conn->query($selectApplicantDetails);
+                $resultApplicantDetails = $execApplicantDetails->fetch_assoc();
+
+
+
+                $selectAddress = ' SELECT `ID`, `USER_ID`, `ADDRESS`, `POSITION`, `MOBILE_NO`, `EMAIL_ADDRESS`, `GOV_AGENCY_NAME`, `GOV_ESTB_NAME`, `DATE_REGISTERED`, `PROVINCE`, `CITY_MUNICIPALITY`, `GOV_NATURE_NAME` FROM `tbl_userinfo` WHERE `USER_ID` = "'.$resultApplication['user_id'].'" ';
+                $execAddress = $conn->query($selectAddress);
+                $resultAddress = $execAddress->fetch_assoc();
+               ?>
+                <tr class="clickable-row" data-href="establishment-profile.php?unique_id=<?php echo $resultApplication['user_id']; ?>">
                         <td class="align-middle">
-                            <a href="establishment-profile.php" target="_blank" class="">
+                            <a href="establishment-profile.php?unique_id=<?php echo $resultApplication['user_id']; ?>" target="_blank" class="">
                                 <div class="font-weight-bold">
-                                    Bureau of Permits
+                                   <?php echo $resultAddress['GOV_AGENCY_NAME']; ?>
                                 </div>
                             </a>
                         </td>
                         <td class="align-middle">
-                            <a href="establishment-profile.php" class="">
+                            <a href="establishment-profile.php?unique_id=<?php echo $resultApplication['user_id']; ?>" class="">
                                 <div class="font-weight-bold">
-                                    Bureau of Permits
+                                   <?php echo $resultAddress['GOV_ESTB_NAME']; ?>
                                 </div>
                             </a>
                         </td>
                         <td class="align-middle">
-                            Room 110, Manila City Hall, Padre Burgos St., Ermita, Barangay 659-A, CITY OF MANILA, NCR, FIRST DISTRICT, NATIONAL CAPITAL REGION
+                             <?php echo $resultAddress['ADDRESS']; ?>
                         </td>
                         <td class="align-middle" nowrap="">
-                                NCR-2021-0000065
+                             <?php echo $resultApplication['control_no']; ?>
                         </td>
                         <td class="align-middle" nowrap="">
                             <span class="label label-lg label-light-success label-inline font-weight-bold py-4">
                                 <i class="la la-clipboard-check mr-2"></i>
-                                June 1, 2021
+                             <?php echo date('F d, Y',strtotime($resultApplication['date_approved'])); ?>
                             </span>
                         </td>
                         <td class="align-middle" nowrap="">
                             <span class="label label-lg label-light-success label-inline font-weight-bold py-4">
                                 <i class="la la-clipboard-check mr-2"></i>
-                                December 1, 2021
+                             <?php echo date('F d, Y', strtotime("+6 months", strtotime($resultApplication['date_approved']))); ?>
                             </span>
                         </td>
                         <td class="align-middle" nowrap="">
                             <span class="label label-lg label-light-success label-inline font-weight-bold py-4">
                                 <i class="la la-clipboard-check mr-2"></i>
-                                Passed
+                             <?php echo $resultApplication['status']; ?>
                             </span>
                         </td>
                     </tr>
+
+
+              <?php } ?>
+
+
             </tbody>
         </table>
 
