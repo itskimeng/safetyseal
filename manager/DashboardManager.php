1b1057fed69d21fc02dbf8beb9d59d3b62924592
KimPogi <markkimsacluti10101996@gmail.com>
2021-06-15 08:28:58 +0800

    code ref

--- manager/DashboardManager.php
+++ manager/DashboardManager.php
@@ -10,29 +10,32 @@ class DashboardManager
         $this->conn = mysqli_connect("localhost", "calabarzondilggo_safetysealuser", "'xPR<W5dm$#-[RQf", "calabarzondilggo_safetyseal");
     }
 
-    public function setLGU($province)
-    {   
-        
-            $sql = "SELECT pro.id as id, pro.name, lgu.name as LGU FROM `tbl_province` pro LEFT JOIN tbl_citymun lgu on pro.id = lgu.province where pro.id = $province";
+    public function setBarChartLabel()
+    {
+        $provinces = ['CAVITE', 'LAGUNA', 'BATANGAS', 'RIZAL', 'QUEZON', 'Lucena City'];
+        for ($i = 0; $i < 6; $i++) {
+            $sql = "SELECT pro.name, checklist.status, count(*) as 'count' FROM `tbl_app_checklist` checklist
+                LEFT JOIN tbl_admin_info ai on checklist.user_id = ai.ID
+                LEFT JOIN tbl_province pro on ai.PROVINCE = pro.id 
+                WHERE  pro.name= '$provinces[$i]' and checklist.status='Approved'";
             $query = mysqli_query($this->conn, $sql);
-            while ($row = mysqli_fetch_assoc($query)) {
-            $data[] = [
-                "LGU" => $row['LGU']
-            ];
+            $result = $row = mysqli_fetch_assoc($query);
+            $data[$provinces[$i]]= $row['count'];
         }
-        
 
         return $data;
     }
 
-    public function getdataForReceived($province)
+    
+    public function getdataForReceived()
     {
        $months =  [0,1,2,3,4,5,6,7,8,9,10,11,12];
         for($i=1; $i < count($months); $i++){
             $sql = "SELECT pro.name as 'PROVINCE' , checklist.status, count(*) as 'count', checklist.date_created FROM `tbl_app_checklist` checklist
             LEFT JOIN tbl_admin_info ai on checklist.user_id = ai.ID
             LEFT JOIN tbl_province pro on ai.PROVINCE = pro.id 
-            WHERE ai.PROVINCE= '$province' and MONTH(checklist.date_created) = $i and checklist.status='For Receiving'";
+            WHERE MONTH(checklist.date_created) = $i and checklist.status='For Receiving'";
+            //  ai.PROVINCE= '$province' and 
             $query = mysqli_query($this->conn, $sql);
             $result = $row = mysqli_fetch_assoc($query);
             $data[$months[$i]]= $row['count'];
@@ -40,7 +43,7 @@ class DashboardManager
 
         return $data;
     }
-    public function getdataApproved($province)
+    public function getdataApproved()
     {
 
         $months =  [0,1,2,3,4,5,6,7,8,9,10,11,12];
@@ -48,7 +51,8 @@ class DashboardManager
             $sql = "SELECT pro.name as 'PROVINCE' , checklist.status, count(*) as 'count', checklist.date_created FROM `tbl_app_checklist` checklist
             LEFT JOIN tbl_admin_info ai on checklist.user_id = ai.ID
             LEFT JOIN tbl_province pro on ai.PROVINCE = pro.id 
-            WHERE ai.PROVINCE= '$province' and MONTH(checklist.date_created) = $i and checklist.status='Approved'";
+            WHERE MONTH(checklist.date_created) = $i and checklist.status='Approved'";
+            // ai.PROVINCE= '$province' and 
             $query = mysqli_query($this->conn, $sql);
             $result = $row = mysqli_fetch_assoc($query);
             $data[$months[$i]]= $row['count'];
