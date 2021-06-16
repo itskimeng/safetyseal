24d3c14ee4b0460c71b2168900dbc6f4191e84aa
sunonin <sodsodjomarie@gmail.com>
2021-06-17 05:11:31 +0800

    added new page for email verification

---
+++ verification_page.php
@@ -0,0 +1,13 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once 'layout/header.html.php'; ?>
+<body >
+    <main>
+        <?php require_once 'layout/navbar.php'; ?>
+        <?php require_once 'views/website/application/verification_landing_page.php'; ?>
+    </main>
+</body>
+
+<?php require_once 'layout/footer.html.php'; ?>
+</html>
