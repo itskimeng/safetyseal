8b34b537e683af2071c7de7f5d9cec39b8325476
sunonin <sodsodjomarie@gmail.com>
2021-06-17 05:24:30 +0800

    code refactor

--- entity/verify_gdrive_user.php
+++ entity/verify_gdrive_user.php
@@ -8,7 +8,8 @@ require_once 'google-api-php-client/src/Google_Client.php';
 require_once 'google-api-php-client/src/contrib/Google_DriveService.php';
 
 $app = new ApplicationManager();
-$client = getGoogleClient('https://localhost/safetyseal/verification_page.php');
+// $client = getGoogleClient('https://localhost/safetyseal/verification_page.php');
+$client = getGoogleClient('https://safetyseal.calabarzon.dilg.gov.ph/verification_page.php');
 
 if (isset($_POST['token_id'])) {
 	$_SESSION['ss_id'] = $_POST['token_id'];
