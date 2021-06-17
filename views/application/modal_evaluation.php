1ce94d605c398d95bd6e0320c29fceae3555bee8
sunonin <sodsodjomarie@gmail.com>
2021-06-17 17:31:30 +0800

    code refactor

--- views/application/modal_evaluation.php
+++ views/application/modal_evaluation.php
@@ -4,16 +4,16 @@
     <div class="modal-content">
       <!-- <form method="POST" action="f"></form> -->
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel"><i class="fa fa-question-circle" aria-hidden="true"></i> Confirmation</h5>
         <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button> -->
       </div>
       <div class="modal-body">
-        Tuloy mo na...
+        Are you sure you want to submit this assessment?
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary btn-close" data-dismiss="modal"><i class="fa fa-window-close"></i> Close</button>
+        <button type="button" class="btn btn-secondary btn-close" data-dismiss="modal"><i class="fa fa-window-close"></i> Cancel</button>
         <button type="button" class="btn btn-primary btn-save_application"><i class="fa fa-step-forward"></i> Proceed</button>
       </div>
     </div>
