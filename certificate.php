68a060b41b08dbe58c2041824c3cedf3bda39612
Jeck Castillo <breezelback@gmail.com>
2021-06-17 12:15:19 +0800

    Update certificate.php
    Code refactor

--- certificate.php
+++ certificate.php
@@ -31,41 +31,37 @@ require 'fpdf/roundedRect1.php';
 
 $pdf = new PDF();
 $pdf->AddPage();
+$pdf->RoundedRect(25, 250, 161, 42, 7, '1234', 'D');
+$pdf->Image('fpdf/disiplina.png',85,1,50);
+$pdf->Image('fpdf/safetyseallogo2.png',8,-26,193.5);
+
+
+$pdf->Image('https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=http%3A%2F%2Festablishment-profile?unique_id='.$resultApplication['user_id'].'%2F&choe=UTF-8',30,255,30,0,'png');
+$pdf->Image('fpdf/dilg.png',62,255,30);
+
 $pdf->SetFont('Arial','',12);
-$pdf->Cell(10,10,'Hello World!');
+$pdf->SetXY(95,254);
+$pdf->Cell(10,10,'Safety Seal No : ');
 
+$pdf->SetFont('Arial','U',14);
+$pdf->SetXY(126,254);
+$pdf->Cell(10,10,$resultApplication['safety_seal_no']);
 
-// $pdf->RoundedRect(25, 250, 161, 42, 7, '1234', 'D');
-// $pdf->Image('fpdf/disiplina.png',85,1,50);
-// $pdf->Image('fpdf/safetyseallogo2.png',8,-26,193.5);
+$pdf->SetFont('Arial','',12);
+$pdf->SetXY(95,262);
+$pdf->Cell(10,10,'Date Issued : ');
 
+$pdf->SetFont('Arial','U',14);
+$pdf->SetXY(126,262);
+$pdf->Cell(10,10,date('F d, Y',strtotime($resultApplication['date_approved'])));
 
-// $pdf->Image('https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=http%3A%2F%2Festablishment-profile?unique_id='.$resultApplication['user_id'].'%2F&choe=UTF-8',30,255,30,0,'png');
-// $pdf->Image('fpdf/dilg.png',62,255,30);
+$pdf->SetFont('Arial','',12);
+$pdf->SetXY(95,275);
+$pdf->Cell(1,1,'Valid Until : ');
 
-// $pdf->SetFont('Arial','',12);
-// $pdf->SetXY(95,254);
-// $pdf->Cell(10,10,'Safety Seal No : ');
-
-// $pdf->SetFont('Arial','U',14);
-// $pdf->SetXY(126,254);
-// $pdf->Cell(10,10,$resultApplication['safety_seal_no']);
-
-// $pdf->SetFont('Arial','',12);
-// $pdf->SetXY(95,262);
-// $pdf->Cell(10,10,'Date Issued : ');
-
-// $pdf->SetFont('Arial','U',14);
-// $pdf->SetXY(126,262);
-// $pdf->Cell(10,10,date('F d, Y',strtotime($resultApplication['date_approved'])));
-
-// $pdf->SetFont('Arial','',12);
-// $pdf->SetXY(95,275);
-// $pdf->Cell(1,1,'Valid Until : ');
-
-// $pdf->SetFont('Arial','U',14);
-// $pdf->SetXY(126,275);
-// $pdf->Cell(1,1,date('F d, Y', strtotime("+6 months", strtotime($resultApplication['date_approved']))));
+$pdf->SetFont('Arial','U',14);
+$pdf->SetXY(126,275);
+$pdf->Cell(1,1,date('F d, Y', strtotime("+6 months", strtotime($resultApplication['date_approved']))));
 
 
 $pdf->Output();
