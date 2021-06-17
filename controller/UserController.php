2aa3210167e2c5e235649a67947125fd1159db29
sunonin <sodsodjomarie@gmail.com>
2021-06-17 15:58:03 +0800

    merge conflict

--- controller/UserController.php
+++ controller/UserController.php
@@ -7,6 +7,7 @@ $app = new UserManager();
 $id = $_SESSION['userid'];
 
 $user_est = $app->fetchEstablishments($id);
-
+$gcode = isset($_SESSION['gcode']) ? $_SESSION['gcode'] : '';
+$gscope = isset($_SESSION['gscope']) ? $_SESSION['gscope'] : '';
 
 ?>
