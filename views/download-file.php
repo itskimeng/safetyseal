72e75c5bf01e3df8cf06dc0f8a8713cfdf07d23b
Jeck Castillo <breezelback@gmail.com>
2021-06-23 14:52:17 +0800

    Update download-file.php
    code ref

--- views/download-file.php
+++ views/download-file.php
@@ -1,24 +1,37 @@
 <?php 
 
-$pdfFile = str_replace(' ', '+', $_GET['filename']);
-
-// $pdfFile = 'http://safetyseal.calabarzon.dilg.gov.ph/files/guidelines/'.$pdfFile;
-$pdfFile = 'C:/xampp/htdocs/safetyseal/files/guidelines/'.$pdfFile; 
-
-//Set the Content-Type to application/pdf
+$pdfFile = $_GET['filename'];
+$pdfFile = str_replace(' ', '+', $pdfFile);
+$file_url = 'C:/xampp/htdocs/safetyseal/files/guidelines/'.$pdfFile ;
 header('Content-Type: application/pdf');
-
-//Set the Content-Length header.
-header('Content-Length: ' . filesize($pdfFile));
-
-//Set the Content-Transfer-Encoding to "Binary"
-header('Content-Transfer-Encoding: Binary');
+header("Content-Transfer-Encoding: Binary"); 
+header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
+readfile($file_url); 
 
 
-//Set the Content-Disposition to attachment and specify
-//the name of the file.
-header('Content-Disposition: attachment; filename=' . $_GET['filename']);
+// $pdfFile = str_replace(' ', '+', $_GET['filename']);
 
-//Read the PDF file data and exit the script.
-readfile($pdfFile);
-exit;
+// $pdfFile = 'C:/xampp/htdocs/safetyseal/files/guidelines/'.$pdfFile ;
+// // echo "<br>";
+// // echo $pdfFile = 'C:/xampp/htdocs/safetyseal/files/guidelines/IA+Safety+Seal+Toolkit.pdf';
+// // echo "<br>";
+// // echo $pdfFile = 'C:/xampp/htdocs/safetyseal/files/guidelines/Joint+Memorandum+Circular+No+.21-01.pdf';
+// // // echo $pdfFile = 'safetyseal/'.$_GET['filename'];
+
+// //Set the Content-Type to application/pdf
+// header('Content-Type: application/pdf');
+
+// //Set the Content-Length header.
+// header('Content-Length: ' . filesize($pdfFile));
+
+// //Set the Content-Transfer-Encoding to "Binary"
+// header('Content-Transfer-Encoding: Binary');
+
+
+// //Set the Content-Disposition to attachment and specify
+// //the name of the file.
+// header('Content-Disposition: attachment; filename=' . $pdfFile);
+
+// //Read the PDF file data and exit the script.
+// readfile($pdfFile);
+// exit;
