c394ff2c82fcd4e851c1387a7a2015df1391173f
KimPogi <markkimsacluti10101996@gmail.com>
2021-06-28 11:41:59 +0800

    code ref

--- entity/post_application.php
+++ entity/post_application.php
@@ -30,39 +30,39 @@ if ($is_new) {
 	$res = $app->insertChecklist($code, $establishment, $nature, $address, $userid, $today->format('Y-m-d H:i:s'), $token);
 } else {
 	$token = $_POST['token'];
-	$app->updateChecklist($token, $establishment, $nature, $address, $today->format('Y-m-d H:i:s'));			
+	$app->updateChecklist($token, $establishment, $nature, $address, $today->format('Y-m-d H:i:s'));
 }
 
 // if (!$is_new) {
 
-	$parent_id = $app->findChecklist($token);
-	foreach ($checklists as $key => $id) {
-		$check_val = $reason = '';
-		$ulist_id = isset($_POST['ulist_id'][$key]) ? $_POST['ulist_id'][$key] : '';
-		if (isset($_POST['chklist_yes'][$key])) {
-			$check_val = 'yes';
-		} elseif (isset($_POST['chklist_no'][$key])) {
-			$check_val = 'no';
-		} elseif (isset($_POST['chklist_na'][$key])) {
-			$check_val = 'n/a';
-			$reason = $_POST['chklist_reason'][$key];
-		} 
-
-		$data = [
-			'parent_id' => $parent_id,
-			'chklist_id' => $is_new ? $key : $ulist_id,
-			'user_id' => $userid,
-			'answer' => $check_val,
-			'reason' => $reason,
-			'date_created' => $today->format('Y-m-d H:i:s')
-		];
-
-		if ($is_new) {
-			$app->insertChecklistEntry($data);			
-		} else {
-			$app->updateChecklistEntry($data);			
-		}
+$parent_id = $app->findChecklist($token);
+foreach ($checklists as $key => $id) {
+	$check_val = $reason = '';
+	$ulist_id = isset($_POST['ulist_id'][$key]) ? $_POST['ulist_id'][$key] : '';
+	if (isset($_POST['chklist_yes'][$key])) {
+		$check_val = 'yes';
+	} elseif (isset($_POST['chklist_no'][$key])) {
+		$check_val = 'no';
+	} elseif (isset($_POST['chklist_na'][$key])) {
+		$check_val = 'n/a';
+		$reason = $_POST['chklist_reason'][$key];
 	}
+
+	$data = [
+		'parent_id' => $parent_id,
+		'chklist_id' => $is_new ? $key : $ulist_id,
+		'user_id' => $userid,
+		'answer' => $check_val,
+		'reason' => $reason,
+		'date_created' => $today->format('Y-m-d H:i:s')
+	];
+
+	if ($is_new) {
+		$app->insertChecklistEntry($data);
+	} else {
+		$app->updateChecklistEntry($data);
+	}
+}
 // }
 
 // $app->setUserApplicationDate($userid , $today->format('Y-m-d H:i:s'));
@@ -74,27 +74,29 @@ foreach ($notify as $key => $data) {
 	notifyUser($data['email'], $establishment);
 }
 // header('location:../wbstapplication.php?ssid='.$token.'');
-header('location:../wbstapplication.php?ssid='.$token.'&code='.$_SESSION['gcode'].'&scope='.$_SESSION['gscope'].'');
+header('location:../wbstapplication.php?ssid=' . $token . '&code=' . $_SESSION['gcode'] . '&scope=' . $_SESSION['gscope'] . '');
 
 
 
 // header('location:../wbstapplication.php');
 
- function notifyUser($emailAddress,$est){
-        
+function notifyUser($emailAddress, $est)
+{
+
 	$to = $emailAddress;
-	$subject = "Safety Seal";
+	$subject = "Safety Seal Portal";
 	$message = '<html><body>';
-	$message .='
-	<div class="card shadow" style="width:50%">
+	$message .= '
+<div class="container>
+	<div class="card shadow" style="width:18rem">
 		<div class="card-header" style="background-color: #009688; color:#fff;">
 			<img src="http://safetyseal.calabarzon.dilg.gov.ph/frontend/images/logo.png" style="width:50px;height:auto;"/>
-			<img src="http://safetyseal.calabarzon.dilg.gov.ph/frontend/images/calabarzon.png" style="width:50px;height:auto;/>
+			<img src="http://safetyseal.calabarzon.dilg.gov.ph/frontend/images/calabarzon.png" style="width:20;height:auto;/>
 		</div>
 	<div class="card-body">
-	<br>'.$est.'</br> submitted an application for safety seal certification
-
+	<br>' . $est . '</br> submitted an application for safety seal certification
 	</div>
+</div>
 </div>
 ';
 	$message .= '</html></body>';
@@ -103,20 +105,21 @@ header('location:../wbstapplication.php?ssid='.$token.'&code='.$_SESSION['gcode'
 	$headers .= "Content-type:text/html;charset=UTF-800" . "\r\n";
 	mail($to, $subject, $message, $headers);
 }
-function addFlash($type, $message, $title) {
+function addFlash($type, $message, $title)
+{
 	$data = [
-        'type'		=> $type, // or 'success' or 'info' or 'warning'
-        'title'     => $title,
-        'message'	=> $message
-    ];
+		'type'		=> $type, // or 'success' or 'info' or 'warning'
+		'title'     => $title,
+		'message'	=> $message
+	];
 
-    return $data;
+	return $data;
 }
 
-function generateCode($conn, $user) 
+function generateCode($conn, $user)
 {
 	$ccode = '2021';
-	$sql = "SELECT counter, id FROM tbl_config WHERE code = '".$ccode."'";
+	$sql = "SELECT counter, id FROM tbl_config WHERE code = '" . $ccode . "'";
 	$query = mysqli_query($conn, $sql);
 	$result = mysqli_fetch_array($query);
 
@@ -129,22 +132,21 @@ function generateCode($conn, $user)
 	if ($cc > 9999) {
 		$new_counter = $cc;
 	} elseif ($cc > 999) {
-		$new_counter = '0'.$cc;
+		$new_counter = '0' . $cc;
 	} elseif ($cc < 10) {
-		$new_counter = '0000'.$cc;
+		$new_counter = '0000' . $cc;
 	} elseif ($cc < 99) {
-		$new_counter = '000'.$cc;
-	} elseif ($cc > 99 AND $cc <= 999) {
-		$new_counter = '00'.$cc;
+		$new_counter = '000' . $cc;
+	} elseif ($cc > 99 and $cc <= 999) {
+		$new_counter = '00' . $cc;
 	}
 
 	$cc = $cc + 1;
 
-	$sql = "UPDATE tbl_config SET counter = '".$cc."' WHERE id = ".$result['id']."";
-    $result = mysqli_query($conn, $sql);
+	$sql = "UPDATE tbl_config SET counter = '" . $cc . "' WHERE id = " . $result['id'] . "";
+	$result = mysqli_query($conn, $sql);
 
-    $control_no = $ccode.'-'.$new_counter;
-   
+	$control_no = $ccode . '-' . $new_counter;
+
 	return $control_no;
 }
-
