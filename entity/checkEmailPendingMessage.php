f3ef071b2c3bdb2ba000f55d177271f53af7107a
KimPogi <markkimsacluti10101996@gmail.com>
2021-06-30 15:39:36 +0800

    send txt

--- entity/checkEmailPendingMessage.php
+++ entity/checkEmailPendingMessage.php
@@ -1,7 +1,7 @@
 <?php
 include '../application/config/connection.php';
 
-    $sqlQuery = "SELECT mc.IP_ADDRESS, ai.CMLGOO_NAME as name, ui.GOV_ESTB_NAME as est, cl.status,cl.email_sending_status, cl.control_no,cl.safety_seal_no, cl.user_id, cl.contact_details from tbl_app_checklist cl 
+    $sqlQuery = "SELECT ai.CMLGOO_NAME,cl.establishment as est, ai.PROVINCE, ai.LGU, cl.email_sending_status, mc.IP_ADDRESS, cl.control_no from tbl_app_checklist cl 
     LEFT JOIN tbl_admin_info ai on cl.user_id = ai.id 
     LEFT JOIN tbl_userinfo ui on ai.id = ui.USER_ID 
     LEFT JOIN tbl_ipadd_details mc 
@@ -9,19 +9,30 @@ include '../application/config/connection.php';
     where email_sending_status = 1 ";
     $result = mysqli_query($conn, $sqlQuery);
 
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_array($result)) {
-            echo json_encode(array(
-                "ip_address" => $row['IP_ADDRESS'],
-                "for_sending" => $row['email_sending_status'],
-                "user" => $row['user_id'],
-                "control_no" => $row['control_no'],
-                "ss_no" => $row['safety_seal_no'],
-                "name" => $row['name'],
-                "est" => $row['est'],
-                "contact_details" => $row['contact_details'],
-                "content" => "Hi  ".$row['name']."!, ".strtoupper($row['est'])."applied for Safety Seal Certification with CTRL No: ".$row['control_no'].". Kindly login to the portal to proceed with the assessment and issuance of certificate."
-            ));
-        }
+    if ($row1 = mysqli_fetch_array($result)) {
+        $sql = "SELECT ai.CMLGOO_NAME as name, ui.MOBILE_NO as contact_details FROM `tbl_admin_info` ai
+        LEFT JOIN tbl_userinfo ui on ai.id=ui.USER_ID
+        WHERE ai.PROVINCE = ".$row1['PROVINCE']." AND ai.LGU=".$row1['LGU']." and ai.ROLES='admin'  ";
+        $result1 = mysqli_query($conn, $sql);
+        // if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_array($result1)) {
+                echo json_encode(array(
+                    "ip_address" => $row1['IP_ADDRESS'],
+                    "for_sending" => $row1['email_sending_status'],
+                    "control_no" => $row1['control_no'],
+                    "est" => $row1['est'],
+                    "contact_details" => $row['contact_details'],
+                    "content" => "Hi  ".$row['name']."!, ".strtoupper($row1['est'])." applied for Safety Seal Certification with CTRL No: ".$row1['control_no'].". Kindly login to the portal to proceed with the assessment and issuance of certificate."
+                ));
+            }
+        // }
+
+    }
+
+
+
+
+
+  
     
-}
+
--- frontend/js/ajax.js
+++ frontend/js/ajax.js
@@ -37,39 +37,48 @@ $.ajax({
 
 
 
-$.ajax({
-    url: 'entity/checkEmailPendingMessage.php',
-    dataType: 'json',
-    cache: false,
-    success: function (data) {
-        let control_no = data.control_no;
-        let content = data.content;
-        let mobile = data.contact_details;
-        let ip_address = data.ip_address;
-        console.log(ip_address);
-        if (data.for_sending == 1) {    
-            $.ajax({
-                type: "GET",
-                url: "http://"+ip_address+"/send/?pass=&number="+mobile+"&data="+content+"",
-                success: function (data) {
-                    
-                    $.ajax({
-                        type: "POST",
-                        url: "entity/post_sending.php",
-                        data: {
-                            cn:control_no,
-                            has_sent: '0',// successfully sent!
-                        },
-                        success: function (data) {
-                            console.log('SMS Notification: status(success!)');
+
+
+setInterval(function() { 
+    $.ajax({
+        url: 'entity/checkEmailPendingMessage.php',
+        dataType: 'json',
+        cache: false,
+        success: function (data) {
+            let control_no = data.control_no;
+            let content = data.content;
+            let mobile = data.contact_details;
+            if(mobile == null){ mobile = '09551003364';}
+            let ip_address = data.ip_address;
+            console.log("http://"+ip_address+"/send/?pass=&number="+mobile+"&data="+content+"");
+            if (data.for_sending == 1) {    
+                $.ajax({
+                    type: "GET",
+                    url: "http://"+ip_address+"/send/?pass=&number="+mobile+"&data="+content+"",
+                    success: function (data) {
+                        if(data.error)
+                        {
+                            console.log('here')
+                        }else{
+                            $.ajax({
+                                type: "POST",
+                                url: "entity/post_sending.php",
+                                data: {
+                                    cn:control_no,
+                                    has_sent: '0',// successfully sent!
+                                },
+                                success: function (data) {
+                                    console.log('SMS Notification: status(success!)');
+                                }
+                            });
                         }
-                    });
-                }
-            });
-        } else {
-            console.log("fail");
+                        
+                    }
+                });
+            } else {
+                console.log("fail");
+            }
+    
         }
-
-    }
-});
-
+    });
+  }, 5000);
