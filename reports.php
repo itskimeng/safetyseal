18129bac83d1136c99ade62ede4d8505c812ee15
KimPogi <markkimsacluti10101996@gmail.com>
2021-07-01 16:46:22 +0800

    code ref

--- reports.php
+++ reports.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+date_default_timezone_set('Asia/Manila');
+
 require 'phpexcel/Classes/PHPExcel.php';
 require 'application/config/connection.php';
 
@@ -7,80 +9,80 @@ require 'application/config/connection.php';
 $phpExcel = new PHPExcel;
 
 //Setting description, creator and title
-$phpExcel ->getProperties()->setTitle("Certified Establishment Report");
-$phpExcel ->getProperties()->setCreator("Safety Seal Administrator");
-$phpExcel ->getProperties()->setDescription("List of certified establishment");
+$phpExcel->getProperties()->setTitle("Certified Establishment Report");
+$phpExcel->getProperties()->setCreator("Safety Seal Administrator");
+$phpExcel->getProperties()->setDescription("List of certified establishment");
 // Creating PHPExcel spreadsheet writer object
 // We will create xlsx file (Excel 2007 and above)
 $writer = PHPExcel_IOFactory::createWriter($phpExcel, "Excel2007");
 // When creating the writer object, the first sheet is also created
 // We will get the already created sheet
-$sheet = $phpExcel ->getActiveSheet();
+$sheet = $phpExcel->getActiveSheet();
 // Setting title of the sheet
 $sheet->setTitle('Establishment List');
 
 // Creating spreadsheet header
-$sheet ->getStyle('A1:R1')->getFont('Arial Black')->setBold(true)->setSize(16);
-$sheet ->getCell('A1')->setValue('No.');
-$sheet ->getCell('B1')->setValue('Control No.');
-$sheet ->getCell('C1')->setValue('Name of Establishment/ Office/Department/Unit');
-$sheet ->getCell('D1')->setValue('Nature of Establishment/ Office/Department/Unit');
-$sheet ->getCell('E1')->setValue('Address');
-$sheet ->getCell('F1')->setValue('Name of Person-in-Charge');
-$sheet ->getCell('G1')->setValue('Contact Details');
-$sheet ->getCell('H1')->setValue('Issuance of Safety Seal Certification (Input "1" if Yes, Input "0" if No)');
-$sheet ->getCell('I1')->setValue('Date of Issuance of Safety Seal Certificate');
-$sheet ->getCell('J1')->setValue('Reason for Non-Issuance of Safety Seal [If answer in (g) is "0" or No]');
-$sheet ->getCell('K1')->setValue('Mode of Acquisition (Input 1 if "by application", Input 2 if "by regular visit)');
-$sheet ->getCell('L1')->setValue('Posted in Official Website? (Input "1" if Yes, Input "0" if No)');
-$sheet ->getCell('M1')->setValue('Complaints Received, if any');
-$sheet ->getCell('N1')->setValue('NO OF ESTABLISHMENT FOR INSPECTION');
-$sheet ->getCell('O1')->setValue('NO. OF ESTABLISHMENTS FOR EVALUATION');
-$sheet ->getCell('P1')->setValue('NO. OF ESTABLISHMENTS DENIED/REFERRRED TO OTHER AGENCIES');
-$sheet ->getCell('Q1')->setValue('Action Taken');
-$sheet ->getCell('R1')->setValue('Remarks');
+$sheet->getStyle('A1:R1')->getFont('Arial Black')->setBold(true)->setSize(16);
+$sheet->getCell('A1')->setValue('No.');
+$sheet->getCell('B1')->setValue('Control No.');
+$sheet->getCell('C1')->setValue('Name of Establishment/ Office/Department/Unit');
+$sheet->getCell('D1')->setValue('Nature of Establishment/ Office/Department/Unit');
+$sheet->getCell('E1')->setValue('Address');
+$sheet->getCell('F1')->setValue('Name of Person-in-Charge');
+$sheet->getCell('G1')->setValue('Contact Details');
+$sheet->getCell('H1')->setValue('Issuance of Safety Seal Certification (Input "1" if Yes, Input "0" if No)');
+$sheet->getCell('I1')->setValue('Date of Issuance of Safety Seal Certificate');
+$sheet->getCell('J1')->setValue('Reason for Non-Issuance of Safety Seal [If answer in (g) is "0" or No]');
+$sheet->getCell('K1')->setValue('Mode of Acquisition (Input 1 if "by application", Input 2 if "by regular visit)');
+$sheet->getCell('L1')->setValue('Posted in Official Website? (Input "1" if Yes, Input "0" if No)');
+$sheet->getCell('M1')->setValue('Complaints Received, if any');
+$sheet->getCell('N1')->setValue('NO OF ESTABLISHMENT FOR INSPECTION');
+$sheet->getCell('O1')->setValue('NO. OF ESTABLISHMENTS FOR EVALUATION');
+$sheet->getCell('P1')->setValue('NO. OF ESTABLISHMENTS DENIED/REFERRRED TO OTHER AGENCIES');
+$sheet->getCell('Q1')->setValue('Action Taken');
+$sheet->getCell('R1')->setValue('Remarks');
 
 
 $sheet->getStyle('A1:M1')->applyFromArray(
-    array(
-        'fill' => array(
-            'type' => PHPExcel_Style_Fill::FILL_SOLID,
-            'color' => array('rgb' => 'd2fabd')
-        )
-    )
+	array(
+		'fill' => array(
+			'type' => PHPExcel_Style_Fill::FILL_SOLID,
+			'color' => array('rgb' => 'd2fabd')
+		)
+	)
 );
 
 
 $sheet->getStyle('N1:P1')->applyFromArray(
-    array(
-        'fill' => array(
-            'type' => PHPExcel_Style_Fill::FILL_SOLID,
-            'color' => array('rgb' => '45e859')
-        )
-    )
+	array(
+		'fill' => array(
+			'type' => PHPExcel_Style_Fill::FILL_SOLID,
+			'color' => array('rgb' => '45e859')
+		)
+	)
 );
 
 $sheet->getStyle('Q1:R1')->applyFromArray(
-    array(
-        'fill' => array(
-            'type' => PHPExcel_Style_Fill::FILL_SOLID,
-            'color' => array('rgb' => 'd2fabd')
-        )
-    )
+	array(
+		'fill' => array(
+			'type' => PHPExcel_Style_Fill::FILL_SOLID,
+			'color' => array('rgb' => 'd2fabd')
+		)
+	)
 );
 
 $sheet->getStyle("A1:R1")->applyFromArray(
-    array(
-        'borders' => array(
-            'allborders' => array(
-                'style' => PHPExcel_Style_Border::BORDER_THIN,
-                'color' => array('rgb' => 'DDDDDD')
-            )
-        )
-    )
+	array(
+		'borders' => array(
+			'allborders' => array(
+				'style' => PHPExcel_Style_Border::BORDER_THIN,
+				'color' => array('rgb' => 'DDDDDD')
+			)
+		)
+	)
 );
 
-$border = array('borders' => array('allborders' => array('style' => PHPExcel_Style_Border::BORDER_MEDIUM, 'color' => array( 'rgb' => '6a6d6d'))));
+$border = array('borders' => array('allborders' => array('style' => PHPExcel_Style_Border::BORDER_MEDIUM, 'color' => array('rgb' => '6a6d6d'))));
 
 
 //setting width and height
@@ -103,7 +105,7 @@ $sheet->getColumnDimension('O')->setWidth(35);
 $sheet->getColumnDimension('P')->setWidth(35);
 $sheet->getColumnDimension('Q')->setWidth(35);
 $sheet->getColumnDimension('R')->setWidth(35);
-$sheet->getStyle('A1:R1')->getAlignment()->setWrapText(true); 
+$sheet->getStyle('A1:R1')->getAlignment()->setWrapText(true);
 $sheet->getStyle('A1:R1')->applyFromArray($border);
 
 
@@ -123,49 +125,62 @@ $sheet->getStyle('A1:R1')->applyFromArray($border);
 // $x = 1;
 // while ($resultUser = $execUser->fetch_assoc()) 
 // {
-	// $sql = ' SELECT `id`, `control_no`, `user_id`, `agency`, `establishment`, `nature`, `address`, `person`, `contact_details`, `status`, `has_consent`, `date_created`, `date_proceed`, `receiver_id`, `date_received`, `approver_id`, `date_approved`, `safety_seal_no`, `reassessed_by`, `date_reassessed`, `date_modified`, `token`, `application_type` FROM `tbl_app_checklist` WHERE `user_id` = "'.$resultUser['ID'].'" ';
-	$sql = "SELECT ac.control_no ,ac.establishment, ac.nature, ac.address, ac.person, ac.contact_details, ac.date_approved
-	FROM tbl_app_checklist ac 
-	LEFT JOIN tbl_admin_info ai on ai.id = ac.user_id 
-	LEFT JOIN tbl_userinfo ui on ui.user_id = ai.id 
-	WHERE ai.PROVINCE = ".$_GET['province']." 
-	AND ai.LGU = ".$_GET['lgu']." 
-	AND ac.application_type = 'Applied' AND ac.status <> '".$_GET['date_range']."'";
-	echo $sql;
-	exit();
-	$result1 = mysqli_query($conn, $sql);
- $i = 2;
- $x = 1;
+// $sql = ' SELECT `id`, `control_no`, `user_id`, `agency`, `establishment`, `nature`, `address`, `person`, `contact_details`, `status`, `has_consent`, `date_created`, `date_proceed`, `receiver_id`, `date_received`, `approver_id`, `date_approved`, `safety_seal_no`, `reassessed_by`, `date_reassessed`, `date_modified`, `token`, `application_type` FROM `tbl_app_checklist` WHERE `user_id` = "'.$resultUser['ID'].'" ';
+$daterange = explode('-', $_GET['date_range']);
+
+$date_from = new DateTime($daterange[0]);
+$date_to = new DateTime($daterange[1]);
+
+$data = [
+	'date_from' => $date_from->format('Y-m-d 00:00:00'),
+	'date_to' => $date_to->format('Y-m-d 23:59:59')
+];
+$from = $data['date_from'];
+$to = $data['date_to'];
+
+$sql = "SELECT tbl_app_checklist.`id`, `control_no`, `user_id`, `agency`, `establishment`, `nature`, `address`, `person`, `contact_details`, `status`, `has_consent`, `date_created`, `date_proceed`, `receiver_id`, `date_received`, `approver_id`, `date_approved`, `safety_seal_no`, `reassessed_by`, `date_reassessed`, `date_modified`, `token`, `application_type` FROM `tbl_app_checklist` 
+	left join tbl_admin_info ai on tbl_app_checklist.user_id = ai.ID
+	where ai.PROVINCE = 5 and
+	ai.LGU = '18' and 
+	tbl_app_checklist.status =  'Approved' and
+	tbl_app_checklist.date_approved >= '" . $from . "' AND 
+	tbl_app_checklist.date_approved <= '" . $to . "'
+
+	";
+
+$result1 = mysqli_query($conn, $sql);
+$i = 2;
+$x = 1;
 
 while ($row = mysqli_fetch_array($result1)) {
 
-		$sheet ->getStyle('A'.$i.':R'.$i)->getFont()->setBold(false)->setSize(14);
-		$sheet ->getCell('A'.$i)->setValue($x);
-		$sheet ->getCell('B'.$i)->setValue($row['control_no']);
-		$sheet ->getCell('C'.$i)->setValue($row['establishment']);
-		$sheet ->getCell('D'.$i)->setValue($row['nature']);
-		$sheet ->getCell('E'.$i)->setValue($row['address']);
-		$sheet ->getCell('F'.$i)->setValue($row['person']);
-		$sheet ->getCell('G'.$i)->setValue($row['contact_details']);
+	$sheet->getStyle('A' . $i . ':R' . $i)->getFont()->setBold(false)->setSize(14);
+	$sheet->getCell('A' . $i)->setValue($x);
+	$sheet->getCell('B' . $i)->setValue($row['control_no']);
+	$sheet->getCell('C' . $i)->setValue($row['establishment']);
+	$sheet->getCell('D' . $i)->setValue($row['nature']);
+	$sheet->getCell('E' . $i)->setValue($row['address']);
+	$sheet->getCell('F' . $i)->setValue($row['person']);
+	$sheet->getCell('G' . $i)->setValue($row['contact_details']);
 
-		$sheet ->getCell('H'.$i)->setValue('1'); //Approved
+	$sheet->getCell('H' . $i)->setValue('1'); //Approved
 
-		$sheet ->getCell('I'.$i)->setValue(date('F, d, Y',strtotime($row['date_approved'])));
-		$sheet ->getCell('J'.$i)->setValue('');
-		$sheet ->getCell('K'.$i)->setValue('1');
-		$sheet ->getCell('L'.$i)->setValue('1');
-		$sheet ->getCell('M'.$i)->setValue('');
-		$sheet ->getCell('N'.$i)->setValue('');
-		$sheet ->getCell('O'.$i)->setValue('');
-		$sheet ->getCell('P'.$i)->setValue('');
-		$sheet ->getCell('Q'.$i)->setValue('');
-		$sheet ->getCell('R'.$i)->setValue('');
+	$sheet->getCell('I' . $i)->setValue(date('F, d, Y', strtotime($row['date_approved'])));
+	$sheet->getCell('J' . $i)->setValue('');
+	$sheet->getCell('K' . $i)->setValue('1');
+	$sheet->getCell('L' . $i)->setValue('1');
+	$sheet->getCell('M' . $i)->setValue('');
+	$sheet->getCell('N' . $i)->setValue('');
+	$sheet->getCell('O' . $i)->setValue('');
+	$sheet->getCell('P' . $i)->setValue('');
+	$sheet->getCell('Q' . $i)->setValue('');
+	$sheet->getCell('R' . $i)->setValue('');
 
-		//set borders
-		$sheet->getStyle('A'.$i.':'.'R'.$i)->applyFromArray($border);
+	//set borders
+	$sheet->getStyle('A' . $i . ':' . 'R' . $i)->applyFromArray($border);
 
-		$i++;
-		$x++;
+	$i++;
+	$x++;
 }
 
 
