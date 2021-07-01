842701bcfd3c78f6550a66158b4a14af7e6b30c0
KimPogi <markkimsacluti10101996@gmail.com>
2021-07-01 11:59:33 +0800

    code ref

--- views/application/report/filter.php
+++ views/application/report/filter.php
@@ -45,9 +45,9 @@
               <option></option>
                 <?php foreach ($citymun_opts as $key => $opts): ?>
                   <?php if ($opts['code'] == $citymun): ?>
-                    <option value="<?php echo $key; ?>" data-province="<?php echo $opts['province']; ?>" data-code="<?php echo $opts['code']; ?>" selected><?php echo $opts['name']; ?></option>
+                    <option value="<?php echo $key; ?>" data-province="<?php echo $opts['code']; ?>" data-code="<?php echo $opts['code']; ?>" selected><?php echo $opts['name']; ?></option>
                   <?php else: ?>
-                    <option value="<?php echo $key; ?>" data-province="<?php echo $opts['province']; ?>" data-code="<?php echo $opts['code']; ?>"><?php echo $opts['name']; ?></option>
+                    <option value="<?php echo $key; ?>" data-province="<?php echo $opts['code']; ?>" data-code="<?php echo $opts['code']; ?>"><?php echo $opts['name']; ?></option>
                   <?php endif ?>
                 <?php endforeach ?>
               </select>
