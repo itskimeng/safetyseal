0204c45d28d975b302769cf7cf8d2ed9e82a8c71
Jeck Castillo <breezelback@gmail.com>
2021-07-05 13:30:28 +0800

    index
    added newly certified establishment

--- index.php
+++ index.php
@@ -8,6 +8,33 @@
                 <?php include 'views/homepage.html.php'; ?>
         </div>
     </main>
+
+
+    <script>
+		$(document).ready(function(){
+			$(".owl-carousel").owlCarousel({
+				loop:true,
+				margin:20,
+			    autoplay:true,
+			    autoplayTimeout:3000,
+			    autoplayHoverPause:true,
+				responsive:{
+					0:{
+						items:1,
+						loop:true
+					},
+					600:{
+						items:3,
+						loop:true
+					},
+					1000:{
+						items:3,
+						loop:true
+					}
+				}
+			});
+		});
+    </script>
 </body>
 <?php include 'layout/footer.html.php'; ?>
 </html>
