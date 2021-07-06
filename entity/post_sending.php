2ad29a0fbce05ec19e2be9a15a20681db0bbac32
KimPogi <markkimsacluti10101996@gmail.com>
2021-07-06 08:58:41 +0800

    code ref

--- entity/post_sending.php
+++ entity/post_sending.php
@@ -6,15 +6,17 @@ require '../application/config/connection.php';
 $has_sent = $_POST['has_sent'];
 $control_no = $_POST['cn'];
 
-
+if($_POST['type'] =='client')
+{
     $sql = "UPDATE `tbl_app_checklist` SET `sms_sending_status` = '$has_sent' where control_no ='$control_no'";
     $query = mysqli_query($conn, $sql);
 
-
-
-    // $sql = "UPDATE `tbl_app_checklist` SET `email_sending_status` = '$has_sent', `pnp_sending_status` = '$has_sent', `bfp_sending_status` = '$has_sent' where control_no ='$control_no'";
-    // $query = mysqli_query($conn, $sql);
-
+}
+else if($_POST['type'] == 'admin')
+{
+    $sql = "UPDATE `tbl_app_checklist` SET `email_sending_status` = '$has_sent', `pnp_sending_status` = '$has_sent', `bfp_sending_status` = '$has_sent' where control_no ='$control_no'";
+    $query = mysqli_query($conn, $sql);
+}
   
 
    ?>
