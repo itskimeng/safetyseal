31133a893b718dbab135bdb31eba5451ef008c7d
sunonin <sodsodjomarie@gmail.com>
2021-07-14 08:53:28 +0800

    code refactor

--- entity/redirect.php
+++ entity/redirect.php
@@ -4,6 +4,7 @@ session_start();
 $url_array = explode('?', 'http://'.$_SERVER ['HTTP_HOST']);
 $url = $url_array[0];
 
+$url = $url.'/safetyseal/admin_verification_page.php';
 
-header("location: ../admin_verification_page.php");    
+header("location: ".$url);    
 // header('location:../admin_verification_page.php');exit;
