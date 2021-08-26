1f759c202fcbf123c8630e65ad37c1f347d24aa4
sunonin <sodsodjomarie@gmail.com>
2021-08-26 11:50:50 +0800

    code refactor

--- session_checker.php
+++ session_checker.php
@@ -2,11 +2,11 @@
 
 
 if (!isset($_SESSION['username'])) {
-	require 'manager/ApplicationManager.php';
-	
-	$am = new ApplicationManager();
-
-	$_SESSION['toastr'] = $am->addFlash('warn', 'Please Login again.', 'Session Expired');
+	$_SESSION['toastr'] = $data = [
+            'type'      => 'warn'
+            'title'     => 'Session Expired',
+            'message'   => 'Please Login again.'
+        ];
 
 	header('location:registration.php');
 }
