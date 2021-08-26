bca8129e56b093004cdd63f23f3cb39e99b366c2
sunonin <sodsodjomarie@gmail.com>
2021-08-26 12:19:03 +0800

    code refactor

--- base_template.php
+++ base_template.php
@@ -1,5 +1,3 @@
-<?php require_once 'session_checker.php'; ?>
-
 <!DOCTYPE html>
 <html>
   <head>
--- dashboard.v2.php
+++ dashboard.v2.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-require_once 'frontend/bower_components/phpti-master/src/ti.php'; 
+  require 'session_checker.php';
+  require_once 'frontend/bower_components/phpti-master/src/ti.php'; 
 ?>
 
 <?php include 'base_template.php'; ?>
