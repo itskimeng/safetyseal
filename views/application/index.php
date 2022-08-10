300271a637a4ff8fa02e390d5c19f968c084dd6c
itskimeng <markkimsacluti10101996@gmail.com>
2022-08-10 13:28:54 +0800

    code ref

--- views/application/index.php
+++ views/application/index.php
@@ -36,7 +36,6 @@
       <div class="modal-content">
         <div class="modal-header bg-warning">
           <h5 class="modal-title" id="exampleModalLabel"><i class="fa fa-file"></i>Application List: (<strong>Draft</strong>)</h5><br>
-          <img src="../../_images/logo.png" style="border:2px solid yellow;" />
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
